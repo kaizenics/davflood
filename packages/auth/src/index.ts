6a4dc4a560bd4c9035a101d7740f08e2c4b958c9
Niko <nicosejohnsoriano@gmail.com>
2026-08-01 05:05:07 +0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,34 @@
+import { expo } from "@better-auth/expo";
+import { createDb } from "@my-better-t-app/db";
+import * as schema from "@my-better-t-app/db/schema/auth";
+import { env } from "@my-better-t-app/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "sqlite",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN, "my-better-t-app://", "exp://", "http://localhost:8081"],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [expo()],
+  });
+}
+
+export const auth = createAuth();
